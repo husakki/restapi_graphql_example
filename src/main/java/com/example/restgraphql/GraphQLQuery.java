5978cd45cd55dd4a916a4b74e6b9a444604919c1
husakki <hueschromo@gmail.com>
2024-08-22 01:36:31 +0200

    simple graphql query

--- src/main/java/com/example/restgraphql/GraphQLQuery.java
+++ src/main/java/com/example/restgraphql/GraphQLQuery.java
@@ -1,4 +1,19 @@
 package com.example.restgraphql;
 
+import com.example.restgraphql.model.Continent;
+import com.example.restgraphql.repository.ContinentRepo;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class GraphQLQuery {
+
+    private final ContinentRepo continentRepo;
+    public GraphQLQuery(ContinentRepo continentRepo) {
+        this.continentRepo = continentRepo;
+    }
+    @QueryMapping
+    Iterable<Continent> continents() {
+        return continentRepo.findAll();
+    }
 }
