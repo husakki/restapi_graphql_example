25688d42c69973bc5fb629a2440c3d0dea11387d
husakki <hueschromo@gmail.com>
2024-08-22 00:29:07 +0200

    init spring project

---
+++ src/main/java/com/example/restgraphql/RestgraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.restgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestgraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestgraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restgraphql/RestgraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestgraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
