3489f174e606d325b26e46bdd0d61b5a0723e097
husakki <hueschromo@gmail.com>
2024-08-22 01:36:11 +0200

    small changes

--- src/main/java/com/example/restgraphql/model/Continent.java
+++ src/main/java/com/example/restgraphql/model/Continent.java
@@ -2,19 +2,21 @@ package com.example.restgraphql.model;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 public class Continent {
 
     @Id
-    @GeneratedValue
     private String continent_code;
 
     private String name;
 
     @OneToMany(mappedBy = "continent", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<Country> countries;
+    private List<Country> countries = new ArrayList<>();
+
+
 
     // Getters and Setters
     public String getContinent_code() {
@@ -29,5 +31,9 @@ public class Continent {
     public Continent() {
     }
 
+    public Continent(String continent_code, String name) {
+        this.continent_code = continent_code;
+        this.name = name;
+    }
 
 }
